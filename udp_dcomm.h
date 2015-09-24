33b2f5bed3d11c80074183d7315f401a526069f6
Rahman Adianto <rahman.adianto@gmail.com>
2015-09-24 16:15:14 +0700

    Add comment

--- udp_dcomm.h
+++ udp_dcomm.h
@@ -6,24 +6,25 @@
 #define _DCOMM_H_
 
 /** ASCII CONST */
-#define SOH 1
-#define STX 2
-#define ETX 3
-#define ENQ 5
-#define ACK 6
-#define BEL 7
-#define CR 13
-#define LF 10
-#define NAK 21
-#define Endfile 26
-#define ESC 27
+#define SOH 1        /* Start of Header Character */
+#define STX 2        /* Start of Text Character */
+#define ETX 3        /* End of Text Character */
+#define ENQ 5        /* Enquiry Character */
+#define ACK 6        /* Acknowledgement */
+#define BEL 7        /* Message Error Warning */
+#define CR 13        /* Carriage Return */
+#define LF 10        /* Line Feed */
+#define NAK 21       /* Negativ Acknowledgement */
+#define Endfile 26   /* End of file Character */
+#define ESC 27       /*ESC key  */
 
 /* XON/XOFF protocol */
 #define XON (0x11)
 #define XOFF (0x13)
 
-#define BYTESIZE 256
-#define MAXLEN 1024
+/* Constant */
+#define BYTESIZE 256  /* The maximum value of a byte */
+#define MAXLEN 1024   /* Maximum messages length */
 
 typedef unsigned char Byte;
 
